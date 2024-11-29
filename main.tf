5a7abfaa86b626ee67fc0889987eacc542cef1ef
Ivan Junior <ivan_junior_we@hotmail.com>
2024-11-29 16:16:05 -0300

    Lista de valores

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "local_file" "foo" {
-  content  = var.conteudo
+  content  = "O máquina que será usada é a ${var.conteudo[0]}"
   filename = "./arquivo.txt"
 }
 
 variable "conteudo" {
-  default = false
-  type = bool
+  default = ["Maquina01", "Maquina02", "Maquina03", "Maquina04"]
+  type = list
   description = "Essa variável contem o conteúdo que vai para o arquivo."
 }
