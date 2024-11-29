40c40bd24da143dc7871097dcf63609e5c8ff85d
Ivan Junior <ivan_junior_we@hotmail.com>
2024-11-29 16:47:53 -0300

    Acerto nos comentários

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "local_file" "foo" {
+  #content  = "O máquina que será usada é a ${join(", ", var.conteudo)}" #adaptado para o set
   content  = "O máquina que será usada é a ${join(", ", var.conteudo)}"
   filename = "./arquivo.txt"
 }
@@ -6,24 +7,24 @@ resource "local_file" "foo" {
 variable "conteudo" {
   
   #Valor string
-  #default = "Conteúdo default do arquivo."
-  #type = string
+  # default = "Conteúdo default do arquivo."
+  # type = string
   
   #Valor numérico
-  #default = 10
-  #type = number
+  # default = 10
+  # type = number
 
   #Valor boolean
-  #default = false
-  #type = bool
+  # default = false
+  # type = bool
   
   #Lista de strings
-  #default = ["Maquina01", "Maquina02", "Maquina03", "Maquina04"]
-  #type = list
+  # default = ["Maquina01", "Maquina02", "Maquina03", "Maquina04"]
+  # type = list
   
   #Lista de algum tipo
-  #default = [1, 2, 3, 4]
-  #type = list(number)
+  # default = [1, 2, 3, 4]
+  # type = list(number)
 
   #Map
   # default = {
@@ -32,10 +33,11 @@ variable "conteudo" {
   #   "grande" = "s-3vcpu-8gb",
   # }
   # type = map
-  #type = map(string) #map de um tipo específico
+  # #type = map(string) #map de um tipo específico
 
-  default = ["s-1vcpu-2gb", "s-2vcpu-4gb", "s-3vcpu-8gb", "s-3vcpu-8gb"] #o set não considera valores repetidos
-  type = set(string)
+  #Set
+  # default = ["s-1vcpu-2gb", "s-2vcpu-4gb", "s-3vcpu-8gb", "s-3vcpu-8gb"] #o set não considera valores repetidos
+  # type = set(string)
   
   description = "Essa variável contem o conteúdo que vai para o arquivo."
 }
