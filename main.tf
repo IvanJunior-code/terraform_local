8e5a894416b58f5f6beefbc73354eb8ff2c3fbb2
Ivan Junior <ivan_junior_we@hotmail.com>
2024-11-29 16:37:08 -0300

    Map

--- main.tf
+++ main.tf
@@ -1,12 +1,38 @@
 resource "local_file" "foo" {
-  content  = "O máquina que será usada é a ${var.conteudo[1]}"
+  content  = "O máquina que será usada é a ${var.conteudo["grande"]}"
   filename = "./arquivo.txt"
 }
 
 variable "conteudo" {
+  
+  #Valor string
+  #default = "Conteúdo default do arquivo."
+  #type = string
+  
+  #Valor numérico
+  #default = 10
+  #type = number
+
+  #Valor boolean
+  #default = false
+  #type = bool
+  
+  #Lista de strings
   #default = ["Maquina01", "Maquina02", "Maquina03", "Maquina04"]
   #type = list
-  default = [1, 2, 3, 4]
-  type = list(number)
+  
+  #Lista de algum tipo
+  #default = [1, 2, 3, 4]
+  #type = list(number)
+
+  #Map
+  default = {
+    "pequena" = "s-1vcpu-2gb",
+    "media" = "s-2vcpu-4gb",
+    "grande" = "s-3vcpu-8gb",
+  }
+  type = map
+  #type = map(string) #map de um tipo específico
+  
   description = "Essa variável contem o conteúdo que vai para o arquivo."
 }
