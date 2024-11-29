0407160c86480911aa035daa784bfec37244fce8
Ivan Junior <ivan_junior_we@hotmail.com>
2024-11-29 16:20:01 -0300

    Lista de numeros

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 resource "local_file" "foo" {
-  content  = "O máquina que será usada é a ${var.conteudo[0]}"
+  content  = "O máquina que será usada é a ${var.conteudo[1]}"
   filename = "./arquivo.txt"
 }
 
 variable "conteudo" {
-  default = ["Maquina01", "Maquina02", "Maquina03", "Maquina04"]
-  type = list
+  #default = ["Maquina01", "Maquina02", "Maquina03", "Maquina04"]
+  #type = list
+  default = [1, 2, 3, 4]
+  type = list(number)
   description = "Essa variável contem o conteúdo que vai para o arquivo."
 }
